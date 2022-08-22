d3c85ed34b6beef3fc44a06dc87b1a532cfce753
yujaba <110364994+yujaba@users.noreply.github.com>
2022-08-21 21:20:09 -0700

    Add files via upload

---
+++ database.php
@@ -0,0 +1,7 @@
+<?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "yunari";
+
+$conn = mysqli_connect($host, $user, $pass, $dbname);
---
+++ editaction.php
@@ -0,0 +1,13 @@
+<?php
+       $id=$_POST['id'];
+       $title=$_POST['title'];
+       
+    include 'database.php';
+    $sql="UPDATE todos SET title='$title' WHERE id=$id";
+    $result=mysqli_query($conn, $sql);
+
+    if($result){
+        header("location: ./index.php");
+
+    }
+?>
